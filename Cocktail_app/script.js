b3d33c31ad11cf2219b9f91768dcee346c357fe3
Valentino Benković <vbenkovic92@gmail.com>
2023-02-26 18:51:56 +0100

    finished

--- Cocktail_app/script.js
+++ Cocktail_app/script.js
@@ -4,17 +4,16 @@
 const input = document.querySelector(`#input-window`);
 const btnSearch = document.querySelector(`#btn-search`);
 const cocktailList = document.querySelector(`.cocktail-list-ordered`);
+const drinkDetails = document.querySelectorAll(`.drink-with-ID`);
 const cocktailDetails = document.querySelector(`#cocktail-details`);
+const allIngredients = document.querySelector(`.list-ingredients`);
 
 // Button function
-btnSearch.addEventListener(`click`, function () {
+const handleButtonSearch = function () {
   const getCocktailName = input.value.trim();
 
   // On each click, refresh the list
   cocktailList.innerHTML = ``;
-
-  // Must have more than 2 letters
-  if (getCocktailName.length < 3) return;
 
   // Get cocktail name path
   const request = new XMLHttpRequest();
@@ -25,25 +24,78 @@ btnSearch.addEventListener(`click`, function () {
     true
   );
   request.send();
-  console.log(request);
+
+  // Must have more than 2 letters and error handling
+  if (getCocktailName.length < 3) {
+    return;
+  }
 
   // JSON part
   request.addEventListener(`load`, function () {
     const data = JSON.parse(this.response);
     const drinksAll = data.drinks;
 
+    console.log(request);
     // For loop
     for (let i = 0; i < drinksAll.length; i++) {
       // Grabbing all drinks from the input
       let drink = data.drinks[i].strDrink;
       let drinkID = data.drinks[i].idDrink;
-      console.log(drink);
 
       // Making new list item in HTML
-      const linkItem = `<li><a src="${drinkID}">${drink}</a></li>`;
+      const linkItem = `<li onclick="handleDrinkDetails(event)" class="drink-with-ID" id="${drinkID}">${drink}</li>`;
 
       // Exporting new list item to HTML
       cocktailList.insertAdjacentHTML(`afterbegin`, linkItem);
     }
   });
+};
+btnSearch.addEventListener(`click`, handleButtonSearch);
+
+// Input works with Enter
+document.addEventListener(`keydown`, function (e) {
+  if (e.key === `Enter`) {
+    handleButtonSearch();
+  }
 });
+
+// Function for getting details from a list item
+const handleDrinkDetails = function (event) {
+  // Grab drink ID
+  const getDrinkID = event.target.id;
+
+  // On each click, refresh the list
+  cocktailDetails.innerHTML = ``;
+
+  // Search drink details by its ID
+  const request = new XMLHttpRequest();
+  request.open(
+    `get`,
+    `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${getDrinkID}
+    `,
+    true
+  );
+  request.send();
+
+  // JSON part
+  request.addEventListener(`load`, function () {
+    const data = JSON.parse(this.response);
+
+    // Making a "look" of cocktail details
+    const allDetails = `
+    <h2>${data.drinks[0].strDrink}</h2>
+    <img src="${data.drinks[0].strDrinkThumb}" alt="Picture of ${data.drinks[0].strDrink}" />
+    <h3>How to instructions:</h3>
+    <p>${data.drinks[0].strInstructions}</p>
+    <h4>Ingredients:</h4>
+    <ul>
+    <li>${data.drinks[0].strIngredient1}</li>
+    <li>${data.drinks[0].strIngredient2}</li>
+    <li>${data.drinks[0].strIngredient3}</li>
+    </ul>
+    `;
+
+    // Exporting new list item to HTML
+    cocktailDetails.insertAdjacentHTML(`afterbegin`, allDetails);
+  });
+};
