fef1f0e659292428df9469afc8d5c7fcd493e308
Valentino Benković <vbenkovic92@gmail.com>
2023-03-01 20:55:28 +0100

    in progress

--- Calculator_app/script.js
+++ Calculator_app/script.js
@@ -7,12 +7,19 @@ const operator = document.querySelectorAll(`.operator`); // - and +
 const decimal = document.querySelector(`.decimal`); // .
 const equal = document.querySelector(`.equal`); // =
 const clear = document.querySelector(`.all-clear`); // clear screen
+let firstComp;
+let secondComp;
+let operation;
+let calculation = Number(firstComp) + operation + Number(secondComp);
 
 // Working numbers
 for (let i = 0; i < number.length; i++) {
   number[i].addEventListener(`click`, function () {
     // Get value from each number and show it in input
-    input.textContent = Number(number[i].dataset.value);
-    //
+    input.textContent = number[i].dataset.value;
+    firstComp = number[i].dataset.value;
+    console.log(firstComp);
   });
 }
+
+// Operators
