9eb0b3a11867e11d4b6064234dbd47e2881f6ac6
Valentino Benković <vbenkovic92@gmail.com>
2023-03-13 19:48:34 +0100

    2.0

--- chatapp-by-react/src/App.js
+++ chatapp-by-react/src/App.js
@@ -3,12 +3,16 @@ import "./App.css";
 
 import MainLogin from "./MainLogIn/MainLogIn";
 import Title from "./Title/Title";
+// import ChatUI from "./UIComponents/ChatUI";
+
+// React router
 
 function App() {
   return (
     <div className="intro hide-intro">
       <Title />
       <MainLogin />
+      {/* <ChatUI /> */}
     </div>
   );
 }
---
+++ chatapp-by-react/src/UIComponents/ChatUI.js
@@ -0,0 +1,11 @@
+import "./ChatUI.css";
+
+export default function ChatUI() {
+  return (
+    <div id="chat-UI">
+      <div className="top-UI">ChatApp</div>
+      <div className="messages-UI"></div>
+      <div className="input-UI"></div>
+    </div>
+  );
+}
